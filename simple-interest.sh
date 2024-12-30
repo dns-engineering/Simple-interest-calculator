2cda036f8b78d796b9a48d33bf00ff004ce5754c
dns-engineering <dhruvsanghani04@gmail.com>
2024-12-30 15:54:38 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Simple Interest Calculator Script
-
 echo "Enter the principal amount:"
 read principal
 
@@ -11,7 +9,6 @@ read rate
 echo "Enter the time (in years):"
 read time
 
-# Calculate simple interest
 interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
 
 echo "The simple interest is: $interest"
